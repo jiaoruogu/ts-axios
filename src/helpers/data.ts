98af8daf61ca18ea9fd20f702444782b584573d6
andychiao <935065201@qq.com>
2020-11-23 15:39:28 +0800

    feat:处理响应data

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -1,8 +1,19 @@
 import { isPlainObject } from './util'
 
 export const transformRequest = (data: any): any => {
-  if(isPlainObject(data)) {
+  if (isPlainObject(data)) {
     data = JSON.stringify(data)
+  }
+  return data
+}
+
+export const transformResponse = (data: any): any => {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data)
+    } catch (e) {
+      // do nothing
+    }
   }
   return data
 }
