b00f4e4c4c5a9ebb94239e664926b847a9ece82e
andychiao <935065201@qq.com>
2020-11-23 13:46:36 +0800

    feat:处理请求header

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import { buildURL } from './helpers/url'
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig): void {
   processConfig(config)
   xhr(config)
 }
 
-function processConfig(config:AxiosRequestConfig):void {
+function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
   config.data = transformRequestData(config)
 }
 
@@ -21,6 +23,11 @@ function transformUrl(config: AxiosRequestConfig): string {
 function transformRequestData(config: AxiosRequestConfig): any {
   const { data } = config
   return transformRequest(data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,25 @@
-import { type } from "os";
+import { type } from 'os'
 
-export type Method = 'get' | 'GET'
-| 'delete' | 'DELETE'
-| 'head' | 'HEAD'
-| 'options' | 'OPTIONS'
-| 'post' | 'POST'
-| 'put' | 'PUT'
-| 'patch' | 'PATCH'
-
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
 
 export interface AxiosRequestConfig {
-  url: string;
-  method?: Method;
-  data?: any;
-  params?: any;
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+  headers?: any
 }
